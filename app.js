e0a08ba3381085c9ddb886f7fc0ede2046297dc1
shitdice <adcockalex@yahoo.com>
2015-11-14 18:56:51 +0000

    Extra shit API key

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var config = {
   app_name: 'Shit Dice',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
-  recaptcha_sitekey: '6LfI_QUTAAAAACrjjuzmLw0Cjx9uABxb8uguLbph',  // <----- EDIT ME!
+  recaptcha_sitekey: '6Lev_RATAAAAANPSUss9BWWz0eClzv9pDf-x6AxT',  // <----- EDIT ME!
   redirect_uri: 'https://shitdice.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
